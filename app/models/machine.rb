e56cef974c42d59b584648b73d7f1f4248593f1f
e1031991 <e1031991@p21mcref01.metavante.com>
2012-10-01 14:29:45 -0500

    Added selective report data presentation by location. It checks if Ziip data or CPU data is available. If so it will display it.

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -2,9 +2,9 @@ class Machine < ActiveRecord::Base
   attr_accessible :id, :name, :serialNumber
   
   has_one :machineConfig, dependent: :destroy
-  has_many :machine_lpars
+  has_many :machine_lpars, dependent: :destroy
   has_many :lpars, through: :machine_lpars
-  has_many :machine_locs
+  has_many :machine_locs, dependent: :destroy
   has_many :locations, through: :machine_locs
   
   validates :name, presence: true, length: { minimum: 1, maximum: 10 }, uniqueness: true
